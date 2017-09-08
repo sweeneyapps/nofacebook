96fa370242b125de9508471c784897b9401d9414
Paul <paul@sweeneyapps.com>
2017-09-07 22:52:01 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# No Facebook
+- A helpful tool to avoid facebook.
+
+---------------------
+## Instruction on how to install this plugins
+
+- clone or download ZIP from this github site
+- on Chrome, go to chrome://extensions/
+- make sure Developer Mode is checked (on upper-right corner of the page)
+- click "Load unpacked extension..." button
+- find the directory with this code that you downloaded.
+- select the directory then click OK
+
+
+## How to use?
+
+- Smiley icon will be loaded w/ "OFF" text
+- You just click on Smiley Icon to turn it "ON"
+- Then you are all set.  No more Facebook for you! 
+- If you want to return to Facebook, just click the Smiley Icon to turn it "OFF"
+- Good luck! 
+
+
+## Instruction on updating if you cloned this repo
+
+### Cloning this repo
+- git clone git@github.com:sweeneyapps/NoFacebook.git
+
+### Updating
+- go to the current directory of NoFacebook
+- git pull
+- Go to chrome://extensions/
+- Look for No Facebook and hit the Reload link
+
+
+---------------------
+
+if u have any suggestions or issues,  leave ur message up on Isssues tab here.
---
+++ main.js
@@ -0,0 +1,86 @@
+// app name: No Facebook
+// author: Paul Sweeney Jr.
+
+var c = {
+  BLACK: [0,0,0,255],
+  GREEN: [34,139,3,255]
+};
+
+var app = {
+  active: true,
+
+  toggleBadge: (on) => {
+    var text  = on ? "ON" : "OFF";
+    var color = on ? c.GREEN : c.BLACK;
+ 
+    chrome.browserAction.setBadgeBackgroundColor({color: color});
+    chrome.browserAction.setBadgeText({text: text});  
+  },
+
+  setup: () => {
+    app.setupEvents(); 
+    app.toggleBadge(true);
+  },
+
+  // setup Chrome Events
+  setupEvents: () => {   
+    chrome.tabs.onCreated.addListener( (id, changeInfo, tab) => {
+      if (reFacebook.test(tab.url)) {
+        app.blockFacebook(tab.id);      
+      }
+    });
+
+    chrome.tabs.onUpdated.addListener( (id, changeInfo, tab) => {
+      if (reFacebook.test(tab.url)) {
+        app.blockFacebook(tab);     
+      } 
+    });
+
+    chrome.idle.setDetectionInterval(60*10);
+    chrome.idle.onStateChanged.addListener(state => { 
+      if (state !== "active") {
+        app.toggleBadge(true);
+      }
+    });
+
+    chrome.browserAction.onClicked.addListener( () => {
+      app.active = !app.active;
+      app.toggleBadge(app.active);
+      chrome.tabs.query({url:"*://*.facebook.com/*"}, (tabs) => {
+        tabs.forEach(tab => {
+          app.blockFacebook(tab);
+        });
+      });
+    });
+  },
+
+  blockFacebook: (tab) => {
+    const reFacebook = /facebook\.com/; 
+    var tabID = tab.id
+
+    if (reFacebook.test(tab.url)) {
+      var code = app.active ? 
+      `
+      var style = document.createElement('style');
+      style.setAttribute('id', 'nofacebookfeed'); 
+      style.innerHTML = 'div[role="feed"] { display:none; }';
+      document.querySelector('body').appendChild(style);
+      `
+      :
+      `
+      var style = document.querySelectorAll('#nofacebookfeed');
+      style.forEach(function(item) {
+        item.remove();
+      });
+      `;
+
+      chrome.tabs.executeScript(tabID, {code: code}, (result) => {
+        if (chrome.runtime.lastError) {  
+        }
+      });
+    }
+  },
+};
+
+app.setup();  // launch point for Chrome Extension to load.
+
